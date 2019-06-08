4ec9bb1bf3043c78220aa573efe762a0b0e74eec
Issarapong Poesua <dekablade01@gmail.com>
2019-06-09 00:56:10 +0700

    Initial Commit

---
+++ Syrup/Syrup.h
@@ -0,0 +1,19 @@
+//
+//  Syrup.h
+//  Syrup
+//
+//  Created by Issarapong Poesua on 9/6/19.
+//  Copyright © 2019 Issarapong Poesua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Syrup.
+FOUNDATION_EXPORT double SyrupVersionNumber;
+
+//! Project version string for Syrup.
+FOUNDATION_EXPORT const unsigned char SyrupVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Syrup/PublicHeader.h>
+
+
